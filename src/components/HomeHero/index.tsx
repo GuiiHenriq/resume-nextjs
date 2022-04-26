62fa7b76c31f297557027d71021219ffb61dfdda
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-04-25 22:04:14 -0300

    create home hero

---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,45 @@
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
+import Avatar from '../../assets/people.webp';
+
+function HomeHero() {
+  return (
+    <Container>
+      <img src={Avatar} alt="Guilherme Henrique" />
+
+      <div>
+        <TextContainer>
+          <h1>Hi</h1>
+          <h2>I am Guilherme</h2>
+        </TextContainer>
+        
+        <InfosContainer>
+          <CodeItem>
+          <span className='comment'>{'\u003C'}-- Resume --{'\u003E'}</span>
+            <span className='purple'>Infos</span> {'\u007B'}
+            <div>
+              Nome: <span className='blue'>Guilherme,</span>
+            </div>
+            <div>
+              Sobrenome: <span className='blue'>Henrique,</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+
+          <CodeItem>
+          <span className='comment'>{'\u003C'}-- Career --{'\u003E'}</span>
+            <span className='purple'>Infos</span> {'\u007B'}
+            <div>
+              Occupation: <span className='blue'>Front-end Developer,</span>
+            </div>
+            <div>
+              Experience: <span className='blue'>4 years,</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
+      </div>
+    </Container>
+  );
+};
+
+export default HomeHero;
