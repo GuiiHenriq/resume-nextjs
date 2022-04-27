c16364cf5f41a1bf63bd3bfe290652e4a43b1810
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-04-26 21:04:39 -0300

    create experience section

---
+++ src/components/Experiences/index.tsx
@@ -0,0 +1,19 @@
+import { Container } from './styles';
+import SectionTitle from '../SectionTitle';
+import ExperienceItem from './ExperienceItem';
+
+function Experiences() {
+  return (
+    <Container>
+      <SectionTitle title="04 years" description="of experience" />
+      
+      <section>
+        <ExperienceItem year="2021" title="Front-end Developer" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+        <ExperienceItem year="2020" title="Front-end Developer" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+        <ExperienceItem year="2019" title="Front-end Developer" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+      </section>
+    </Container>
+  );
+};
+
+export default Experiences;
